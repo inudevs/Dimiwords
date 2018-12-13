900ead2807854c559cd725c18057d10fafd7e07c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-12-14 07:20:52 +0900

    ADD: progress bar

--- Client/src/components/Show.vue
+++ Client/src/components/Show.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <h1>단어 외우기</h1>
-    <input id="guess" v-on:keyup="key"><br>
-    <strong>{{ current.ko }}</strong>
+    <strong id="question">
+      <span v-if="running">{{ current.ko }}</span>
+      <span v-else>끝</span>
+    </strong><br>
+    <div id="progress">
+      <div id="bar"><span id="time">{{ (progress.current/progress.all || 0)*100 + '%' }}</span></div>
+    </div>
+    <input id="guess" v-on:keyup="key">
+    <div v-if="!running">
+      <i class="fas fa-redo"></i>
+      <i class="fas fa-bars"></i>
+    </div>
   </div>
 </template>
 
@@ -14,32 +24,49 @@ export default {
     .then((response) => {
       this.wordbook = response.data;
       this.words = this.wordbook[0].words;
+      this.progress.all = this.words.length;
+      this.progress.current = 0;
       this.current = this.next();
+      this.current.ko = this.current.ko.join(', ');
+      this.running = true;
+      document.getElementById('guess').focus();
+      this.move();
     })
   },
   data: function () {
     return {
       wordbook: {},
       words: {},
-      current: {}
+      current: {},
+      progress: {},
+      running: true
     }
   },
   methods: {
       key: function(event) {
         var _input = document.getElementById('guess');
-        if (_input.value == this.current.en){
-          console.log(true);
+        if (_input.value.toLowerCase() == this.current.en){
+          // console.log(true)
+          _input.value = this.current.en;
+          _input.disabled = true;
           _input.style.backgroundColor = 'lightgreen';
           _input.style.color = 'white';
           setTimeout((src, item) => {
             _input.value = '';
+            _input.disabled = false;
+            _input.focus();
             _input.style.backgroundColor = 'white';
             _input.style.color = 'black';
+            this.move();
+            this.progress.current++;
             if (this.words.length == 0){
               _input.disabled = true;
-              this.current.ko='끝';
+              this.running = false;
             }
-            else this.current = this.next();
+            else {
+              this.current = this.next();
+              this.current.ko = this.current.ko.join(', ');
+            }
           }, 500);
         }
       },
@@ -52,6 +79,18 @@ export default {
       next: function () {
         this.shuffle(this.words);
         return this.words.pop();
+      },
+      move: function (bar) {
+        var bar = document.getElementById('bar');
+        var width = 1;
+        var id = setInterval((src, item) => {
+          if (width >= 100) {
+            clearInterval(id);
+          } else {
+            bar.style.width = (this.progress.current/this.progress.all)*100 + '%';
+            // console.log(this.progress)
+          }
+        }, 100);
       }
   }
 }
