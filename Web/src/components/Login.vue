720bab9ec6344a448153af11ea6ba365f803bce8
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-01-11 02:38:46 +0900

    UPDATE: AD in login

--- Web/src/components/Login.vue
+++ Web/src/components/Login.vue
@@ -11,6 +11,7 @@
             </div>
         </div>
         <button class="login-user" @click="login">로그인</button>
+        <iframe width="728" height="90" allowtransparency="true" src="https://tab2.clickmon.co.kr/pop/wp_ad_728.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1042596%7C%5E%7CAdver_M_1046207&rt_ad_id_code=RTA_105965&mon_rf=https%3A%2F%2Fdimiwords.tk" frameborder="0" scrolling="no"></iframe>
     </div>
 </template>
 
